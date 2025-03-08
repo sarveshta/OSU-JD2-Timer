030f67f07a4013d7156a9d1a41937ac9be300c73
Sarvesh Thiruppathi <sarveshthiruppathi@gmail.com>
2025-03-07 16:09:51 -0800

    updated to not rely on opencv based libraries

--- src/capturePhotos.py
+++ src/capturePhotos.py
@@ -1,18 +1,16 @@
-from picamzero import Camera
 import os
-import random
-import string
+import subprocess
 
 def capture_photo(capture_filename):
     target_dir = os.path.join(os.getcwd(), "captured_images")
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
-    cam = Camera()
-    cam.start_preview()
-    cam.take_photo(os.path.join(target_dir, capture_filename))
-    cam.stop_preview()
+    filepath = os.path.join(target_dir, capture_filename)
+    cmd = ["raspistill", "-o", filepath]
+    subprocess.run(cmd, check=True)
+    return filepath
 
 if __name__ == "__main__":
-    filename = capture_photo()
+    filename = capture_photo("captured_image.jpg")
     print("Photo captured and saved as:", filename)
